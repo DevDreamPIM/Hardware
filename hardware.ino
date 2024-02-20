92c7dc9b95231bac73ef4e5c5918bcf1f2ce1869
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-20 22:10:50 +0100

    Myo Gesture Control Data retreive

--- hardware.ino
+++ hardware.ino
@@ -1,5 +1,6 @@
 // Put your libraries here, to import them:
-#include "BluetoothSerial.h"
+#include "BluetoothSerial.h"  // Nombre de caractères envoyés max est de 455 et prend environt 7 secondes à arriver dans flutter
+#include <myo.h>
 
 // Put your global code here, to declare variables and other:
 // Start Bluetooth Code ***********************************************************************************
@@ -17,15 +18,50 @@ String device_name = "DevDream-SmartWatch";
 
 BluetoothSerial SerialBT;
 // End Bluetooth Code *************************************************************************************
+// Start Myo Gesture Control Code *************************************************************************
+armband myo; // Myo BLE Armband
+
+void print_imu_orientation(myohw_imu_data_t *imu)
+{
+  Serial.print(imu->orientation.x);
+  Serial.print("\t");
+  Serial.print(imu->orientation.y);
+  Serial.print("\t");
+  Serial.print(imu->orientation.z);
+  Serial.print("\t");
+  Serial.print(imu->orientation.w);
+  Serial.print("\t");
+}
+
+void imu_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify)
+{
+  myohw_imu_data_t *imu_data = (myohw_imu_data_t *)pData;
+  print_imu_orientation(imu_data);
+  Serial.println();
+}
+
+void myo_connect()
+{
+  Serial.println("Connecting to Myo device...");
+  myo.connect();
+  Serial.println(" - Connected to Myo device");
+  delay(100);
+
+  myo.set_myo_mode(myohw_emg_mode_none,             // EMG mode
+                   myohw_imu_mode_send_data,        // IMU mode
+                   myohw_classifier_mode_disabled); // Classifier mode
+
+  myo.imu_notification(TURN_ON)->registerForNotify(imu_callback);
+}
+// End Myo Gesture Control Code ***************************************************************************
 
 void setup() { // Put your setup code here, to run once:  +++++++++++++++++++++++++++++++++++++++++++++++++++
   // Start Bluetooth Code ***********************************************************************************
   Serial.begin(115200);
-    SerialBT.begin(device_name); //Bluetooth device name
-    // SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
-    Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  SerialBT.begin(device_name); //Bluetooth device name
+  // SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it to the phone with Bluetooth!\n", device_name.c_str());
   // End Bluetooth Code *************************************************************************************
-
 }
 
 void loop() { // Put your main code here, to run repeatedly:  +++++++++++++++++++++++++++++++++++++++++++++++
@@ -36,7 +72,13 @@ void loop() { // Put your main code here, to run repeatedly:  ++++++++++++++++++
   if (SerialBT.available()) {
     Serial.write(SerialBT.read());
   }
-  delay(20);
   // End Bluetooth Code *************************************************************************************
-
+  // Start Myo Gesture Control Code *************************************************************************
+  if (!myo.connected)
+  {
+    myo_connect();
+  }
+  // End Myo Gesture Control Code ***************************************************************************
+  
+  delay(20);
 }
