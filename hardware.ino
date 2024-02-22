d90260ee28dcc69928c31faaf686da4222d4f203
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-22 22:40:33 +0100

    Split program into multiple files, bluetooth

--- hardware.ino
+++ hardware.ino
@@ -1,23 +1,14 @@
 // Put your libraries here, to import them:
-#include "BluetoothSerial.h"  // Nombre de caractères envoyés max est de 455 et prend environt 7 secondes à arriver dans flutter
+#include "bluetooth.h"
 #include <myo.h>
 
+// Put your external files here, to includes functions:
+#include "bluetooth.h"
+
 // Put your global code here, to declare variables and other:
-// Start Bluetooth Code ***********************************************************************************
-String device_name = "DevDream-SmartWatch";
-
-// Check if Bluetooth is available
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-  #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-// Check Serial Port Profile
-#if !defined(CONFIG_BT_SPP_ENABLED)
-  #error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
-#endif
 
 BluetoothSerial SerialBT;
-// End Bluetooth Code *************************************************************************************
+
 // Start Myo Gesture Control Code *************************************************************************
 armband myo; // Myo BLE Armband
 
@@ -77,30 +68,21 @@ void emg_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pD
 }
 // End Myo Gesture Control Code ***************************************************************************
 
-void setup() { // Put your setup code here, to run once:  +++++++++++++++++++++++++++++++++++++++++++++++++++
-  // Start Bluetooth Code ***********************************************************************************
+void setup() { // Put your setup code here, to run once:
   Serial.begin(115200);
-  SerialBT.begin(device_name); //Bluetooth device name
-  // SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it to the phone with Bluetooth!\n", device_name.c_str());
-  // End Bluetooth Code *************************************************************************************
+  setupBluetooth(SerialBT);
+  
 }
 
-void loop() { // Put your main code here, to run repeatedly:  +++++++++++++++++++++++++++++++++++++++++++++++
-  // Start Bluetooth Code ***********************************************************************************
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  // End Bluetooth Code *************************************************************************************
+void loop() { // Put your main code here, to run repeatedly:
+  handleBluetooth(SerialBT);
+  delay(20);
   // Start Myo Gesture Control Code *************************************************************************
-  if (!myo.connected)
-  {
-    myo_connect();
-  }
+  //if (!myo.connected)
+  //{
+   // myo_connect();
+  //}
+  delay(10);
   // End Myo Gesture Control Code ***************************************************************************
   
-  delay(20);
 }
