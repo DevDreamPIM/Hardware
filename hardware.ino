eb3f49bc50e7b0b77d438334283e9ea157e98d0a
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-24 15:46:29 +0100

    Reading EMG and IMU data from Myo at the same time

--- hardware.ino
+++ hardware.ino
@@ -24,11 +24,11 @@ void loop() { // Put your main code here, to run repeatedly:
   myo_connect();
 
 
-  int* imu = getImuData();
-  Serial.println(imu[0]);
-  Serial.print(imu[1]);
-  Serial.print(imu[2]);
-  Serial.print(imu[3]);
+  // int* imu = getImuData();
+  // Serial.println(imu[0]);
+  // Serial.print(imu[1]);
+  // Serial.print(imu[2]);
+  // Serial.print(imu[3]);
 
 
   delay(40);
--- myo.cpp
+++ myo.cpp
@@ -9,18 +9,6 @@ int* getImuData(){
 }
 
 // IMU Data
-void print_imu_orientation(myohw_imu_data_t *imu)
-{
-  Serial.print(imu->orientation.x);
-  Serial.print("\t");
-  Serial.print(imu->orientation.y);
-  Serial.print("\t");
-  Serial.print(imu->orientation.z);
-  Serial.print("\t");
-  Serial.print(imu->orientation.w);
-  Serial.print("\t");
-}
-
 void imu_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify)
 {
   myohw_imu_data_t *imu_data = (myohw_imu_data_t *)pData;
@@ -28,8 +16,6 @@ void imu_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pD
   imu_data_get[1] = imu_data->orientation.y;
   imu_data_get[2] = imu_data->orientation.z;
   imu_data_get[3] = imu_data->orientation.w;
-  //print_imu_orientation(imu_data);
-  //Serial.println();
 }
 
 // EMG Data (Electromyography)
@@ -46,7 +32,7 @@ void emg_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pD
 {
   myohw_emg_data_t *emg_data = (myohw_emg_data_t *)pData;
   print_emg_sample(emg_data->sample1, myohw_num_emg_sensors);
-  print_emg_sample(emg_data->sample2, myohw_num_emg_sensors);
+  //print_emg_sample(emg_data->sample2, myohw_num_emg_sensors);
 }
 
 void myo_connect()
@@ -57,15 +43,12 @@ void myo_connect()
     Serial.println(" - Connected to Myo device");
     delay(100);
 
-    myo.set_myo_mode(myohw_emg_mode_none,             // EMG mode
+    myo.set_myo_mode(myohw_emg_mode_send_emg,             // EMG mode
                    myohw_imu_mode_send_data,        // IMU mode
                    myohw_classifier_mode_disabled); // Classifier mode
-    // myo.set_myo_mode(myohw_emg_mode_send_emg,         // EMG mode
-    //                  myohw_imu_mode_none,             // IMU mode
-    //                  myohw_classifier_mode_disabled); // Classifier mode
 
     myo.imu_notification(TURN_ON)->registerForNotify(imu_callback);
-    // myo.emg_notification(TURN_ON)->registerForNotify(emg_callback);
+    myo.emg_notification(TURN_ON)->registerForNotify(emg_callback);
   }
   
 }
