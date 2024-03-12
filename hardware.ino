aabfecbf892b097cd059fe074b986d08a03a9606
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-03-12 20:17:20 +0100

    Removed unused wifi and http code

--- hardware.ino
+++ hardware.ino
@@ -5,41 +5,34 @@
 #include "constants.h"
 #include "bluetooth.h"
 #include "myo.h"
-#include "wifi.h"
-#include "httpRequest.h"
 #include "PulseSensorManager.h"
 
 // Put your global code here, to declare variables and other:
 BluetoothSerial SerialBT;
-PulseSensorManager pulseSensorManager(Constants::PULSE_INPUT_PIN,Constants::PULSE_THRESHOLD);
+//PulseSensorManager pulseSensorManager(Constants::PULSE_INPUT_PIN,Constants::PULSE_THRESHOLD);
 
-void sendDataToServer() {
-  int bmp = pulseSensorManager.getBPM();
-  int imu[] = {0, 0, 0, 0};//getImuData(); // 0 -> 3
-  int emg[] = {0, 0, 0, 0, 0, 0, 0, 0};//getEmgData(); // 0 -> 8
-  const char* user = Constants::USER;
-  sendDataToServerHttp(user, imu, emg,bmp);
+void sendDataToPhone() {
+  int bmp = 70;// pulseSensorManager.getBPM();
+  int* imu = getImuData(); // 0 -> 3
+  int* emg = getEmgData(); // 0 -> 8
 }
 
 void setup() { // Put your setup code here, to run once:
   Serial.begin(115200);
- // setupBluetooth(SerialBT);
-  setupWifi();
+  setupBluetooth(SerialBT);
 
-  pulseSensorManager.setup();
+  //pulseSensorManager.setup();
 
 }
 
 void loop() { // Put your main code here, to run repeatedly:
   handleBluetooth(SerialBT);
-  //getTest();
-  //myo_connect();
+  myo_connect();
   // Pulse Sensor
-  pulseSensorManager.update();
-  sendDataToServer();
+  //pulseSensorManager.update();
 
   
  
 
-  delay(1000);
+  delay(40);
 }
