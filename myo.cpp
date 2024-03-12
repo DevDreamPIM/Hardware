538ffee19e87d8f0d8fc8ed15aebe9c43c2d0ade
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-03-12 23:37:41 +0100

    Sending Refined data via BLE every 10 min

--- myo.cpp
+++ myo.cpp
@@ -1,15 +1,15 @@
 #include "myo.h"
 
 armband myo; // Myo BLE Armband
-int imu_data_get[4];
-int emg_data_get[8];
+int imu_data_get;
+int emg_data_get;
 
 //get data for POST Request
-int* getImuData(){
+int getImuData(){
   return imu_data_get;
 }
 
-int* getEmgData(){
+int getEmgData(){
   return emg_data_get;
 }
 
@@ -17,23 +17,17 @@ int* getEmgData(){
 void imu_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify)
 {
   myohw_imu_data_t *imu_data = (myohw_imu_data_t *)pData;
-  imu_data_get[0] = imu_data->orientation.x;  //takes less space than for loop
-  imu_data_get[1] = imu_data->orientation.y;
-  imu_data_get[2] = imu_data->orientation.z;
-  imu_data_get[3] = imu_data->orientation.w;
+  // imu_data_get[0] = imu_data->orientation.x;
+  // imu_data_get[1] = imu_data->orientation.y;
+  // imu_data_get[2] = imu_data->orientation.z;
+  imu_data_get = imu_data->orientation.w; //C'est le mouvement le plus important celon notre besoin, il aide à détecter les mouvement de bras lors d'un footing
 }
 
 void emg_callback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify)
 {
   myohw_emg_data_t *emg_data = (myohw_emg_data_t *)pData;
-  emg_data_get[0] = emg_data->sample1[0];
-  emg_data_get[1] = emg_data->sample1[1];
-  emg_data_get[2] = emg_data->sample1[2];
-  emg_data_get[3] = emg_data->sample1[3];
-  emg_data_get[4] = emg_data->sample1[4];
-  emg_data_get[5] = emg_data->sample1[5];
-  emg_data_get[6] = emg_data->sample1[6];
-  emg_data_get[7] = emg_data->sample1[7];
+  emg_data_get = (emg_data->sample1[0]+emg_data->sample1[1]+emg_data->sample1[2]+emg_data->sample1[3]
+  +emg_data->sample1[4]+emg_data->sample1[5]+emg_data->sample1[6]+emg_data->sample1[7])/8; //Regrouper les 8 valeurs en une
 }
 
 void myo_connect()
