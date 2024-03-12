1460daee861e161e96b05e852b0017f05934196d
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-03-12 23:58:33 +0100

    New pulse update function & Deactivation of live monitoring

--- hardware.ino
+++ hardware.ino
@@ -19,20 +19,29 @@ int lastIndex = 0;
 long initialTime = millis();
 long initialSommeTime = initialTime;
 
+bool SeizureMonitoringOn = true;
+
 void dataProcessing() {
   //get sensor data for this instant
-  int bpm = 70;// pulseSensorManager.getBPM();
+  int bpm = 210;// pulseSensorManager.getBPM();
   int imu = getImuData(); // axe w
   int emg = getEmgData(); //  8 en un
 
+  //activer et desactiver la detection de crise
+  if (SerialBT.available()) {
+    if(SerialBT.read()){
+      SeizureMonitoringOn = SerialBT.read() == "true";
+    }
+  }
+
   //ici ajouter la detection de crise en temps reel (avec variable Monitoring actif)
-  if(bpm > 200 && emg > 35){
+  if(bpm > 200 && emg > 35 && SeizureMonitoringOn){
     //déclencher le buzzer
 
     //déclancher l'alerte sur l'ecran
 
     //envoie de l'alerte crise par BLE avec le mot clef cri
-    SerialBT.write("cri");
+    SerialBT.write((const uint8_t *)"cri", 3); // "cri" is 3 characters long
   }
 
   // Vérifier l'intervalle de 10 secondes
@@ -51,9 +60,9 @@ void dataProcessing() {
   // ici, toutes les 10 minutes (index = 60),envoyer et vider les tableaux
   if (lastIndex == 60) {
     //envoyer les données par BLE
-    SerialBT.write(bpmStr);
-    SerialBT.write(emgStr);
-    SerialBT.write(imuStr);
+    SerialBT.write((const uint8_t*)bpmStr.c_str(), bpmStr.length());
+    SerialBT.write((const uint8_t*)emgStr.c_str(), emgStr.length());
+    SerialBT.write((const uint8_t*)imuStr.c_str(), imuStr.length());
     // Réinitialiser le temps initial et l'index du dernier élément utilisé
     initialSommeTime = currentTime;
     initialTime = currentTime;
@@ -76,7 +85,6 @@ void setup() { // Put your setup code here, to run once:
 void loop() { // Put your main code here, to run repeatedly:
   handleBluetooth(SerialBT);
   myo_connect();
-  // Pulse Sensor
   //pulseSensorManager.update();
 
   dataProcessing();
