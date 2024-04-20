87af86ee88502d04261f8edc59923728fee2cb32
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-04-20 18:25:09 +0100

    Fixed Loss of data when sending big shunks

--- constants.cpp
+++ constants.cpp
@@ -5,7 +5,7 @@ namespace Constants {
     const int PULSE_THRESHOLD = 550;
     const char* USER = "609f1519c33d2d001d45e888";
     const char* deviceName = "DevDream-SmartWatch-BT";
-    const char* ssid = "TOPNET_4140";     //TOPNET_4140
-    const char* password = "8p18p2c6f4";  //8p18p2c6f4
-    const char* serverName = "http://192.168.1.16:9090/sensors/";
+    const char* ssid = "g2-esprit";     //TOPNET_4140
+    const char* password = "";  //8p18p2c6f4
+    const char* serverName = "http://172.16.1.182:9090/sensors/";
 }
